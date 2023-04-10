8476ad30ce122219751f523129c8b5887a03f259
Jiadong Jin <jjd1024@foxmail.com>
2023-04-10 13:44:22 +0800

    fini

---
+++ Chessboard.cs
@@ -0,0 +1,106 @@
+﻿//INF1008-TP2-Chessboard.cs
+//Jiadong Jin JINJ86100000
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF1008_tp2
+{
+    public class Chessboard
+    {
+        private int N { get; set; }
+
+        //private Node Root { get; set; }
+
+        private bool[,] Board { get; set; }
+
+        /* NegDiag est un tableau dont les indices indiquent (ligne - colonne + N - 1) et 
+         * qui est utilisé pour vérifier si une reine peut être placée sur une diagonale négative ou non.*/
+        private bool[] NegDiag { get; set; }
+
+        /* PosDiag est un tableau dont les indices indiquent (ligne + colonne) et
+         * qui est utilisé pour vérifier si une reine peut être placée sur la diagonale positive ou non.*/
+        private bool[] PosDiag { get; set; }
+
+        /* Column est un tableau dont les indices indiquent la colonne et
+         * qui est utilisé pour vérifier si une reine peut être placée dans cette colonne ou non.*/
+        private bool[] Column { get; set; }
+
+        public Chessboard(int n)
+        {
+            this.N = n;
+            this.Board = new bool[N, N];
+            NegDiag = new bool[2 * N - 1];
+            PosDiag = new bool[2 * N - 1];
+            Column = new bool[N];
+        }
+
+        //Une fonction d'utilité récursive pour résoudre le problème de N Reines
+        public void solveNReines(bool[,] board, int k, List<List<int>> allSteps)
+        {
+            //Sauvegarde de tous les progrès et étapes en cours
+            List<int> currentStep = Enumerable.Range(0, N).Select(i => Enumerable.Range(0, N).Where(j => board[i, j]).DefaultIfEmpty(-1).First()).Where(x => x != -1).ToList();
+            /*List<int> currentStep = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (board[i, j])
+                    {
+                        currentStep.Add(j);
+                        break;
+                    }
+                }
+            }*/
+            allSteps.Add(currentStep);
+            //Si toutes les reines sont placées, revener et continuer à chercher d'autres méthodes de placement.
+            if (k >= N) return;
+            for (int i = 0; i < N; i++)
+            {
+                // Vérifier que la position actuelle se trouve dans les colonnes des autres reines, ou sur leur diagonale positive ou négative.
+                if ((NegDiag[i - k + N - 1] != true && PosDiag[i + k] != true) && Column[i] != true)
+                {
+                    //Placer cette reine dans le tableau board[i,k]
+                    board[i, k] = true;
+                    NegDiag[i - k + N - 1] = PosDiag[i + k] = Column[i] = true;
+                    //retrouver pour placer le reste des reines
+                    solveNReines(board, k + 1, allSteps);
+                    //RETOUR ARRIÈRE
+                    board[i, k] = false;
+                    NegDiag[i - k + N - 1] = PosDiag[i + k] = Column[i] = false;
+                }
+            }
+        }
+
+        public List<List<int>> Resoudre()
+        {
+            bool[,] board = Board;
+            List<List<int>> allSteps = new();
+            solveNReines(board, 0, allSteps);
+            return allSteps;
+        }
+
+        public void Afficher(List<int> result)
+        {
+            foreach (int i in result)
+            {
+                Console.WriteLine(" " + string.Join(" ", Enumerable.Range(0, result.Count).Select(j => j == i ? 'X' : '+')));
+            }
+            /*for (int i = 0; i < result.Count; i++) {
+                for (int j = 0; j < result.Count; j++)
+                {
+                    char c = '0';
+                    if (j == result[i]) {
+                        c = 'X';
+                    }
+                    Console.Write(c+" ");
+                }
+                Console.WriteLine();
+            }*/
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -1,58 +1,84 @@
-﻿//INF1008-TP1-Main.cs
-//Jiadong Jin JINJ86100000
+﻿// INF1008-TP2-Main.cs
+// Jiadong Jin JINJ86100000
+
+// Programme C# pour résoudre le problème de la reine N en utilisant le backtracking
+using INF1008_tp2;
 
 bool menu_continue = true;
 while (menu_continue)
 {
     Console.WriteLine("\r\nMenu");
-    Console.WriteLine("Veuillez entrer un numéro pour sélectionner la fonction que vous voulez utiliser:");
-    Console.WriteLine("(1) Générer un labyrinthe d'une taille spécifiée");
-    Console.WriteLine("(2) Utiliser l'algorithme de Prim pour résoudre le labyrinthe.");
-    Console.WriteLine("(3) Quitter l'application.");
-    switch (Console.ReadKey().KeyChar)
+
+    Console.WriteLine("Veuillez entrer le N (N reines sur un plateau de jeu N*N) :");
+    if (!int.TryParse(Console.ReadLine(), out int n))
     {
-        case '1':
-            Console.WriteLine("\r\nVeuillez entrer le nombre de colonnes du labyrinthe.:");
-            if (!int.TryParse(Console.ReadLine(), out int y))
-            {
-                Console.WriteLine("Caractère non valide.");
-                break;
-            }
-            Console.WriteLine("\r\nVeuillez entrer le nombre de lignes du labyrinthe.:");
-            if (!int.TryParse(Console.ReadLine(), out int x))
-            {
-                Console.WriteLine("Caractère non valide.");
-                break;
-            }
-            //Initialisation un compteur
-            //pour compter le nombre d'opérations de base utilisées dans la génération des labyrinthes et des listes d'adjacence.
-            //Générer un labyrinthe d'une taille donnée à partir du nombre de lignes et de colonnes saisies par l'utilisateur.
-            Console.WriteLine("Le nombre d'opérations élémentaires (l'initialisation de la liste d'adjacence):");
-            Console.WriteLine("Le nombre d'opérations élémentaires (la génération du labyrinthe):" + "\r\n");
-
-            Console.WriteLine("Le nombre d'opérations élémentaires (l'affichage du labyrinthe):");
-
-            Console.WriteLine("Nombre non valide.");
-            break;
-        case '2':
-            if (true)
-            {
-                //Utilisation de l'algorithme prim pour résoudre des labyrinthes et afficher le nombre des opérations de base
-                Console.WriteLine("\r\nL'arbre couvrant de poids minimal de ce labyrinthe est :");
-                Console.WriteLine("\r\nLe nombre d'opérations élémentaires (l'affichage du labyrinthe):");
-            }
-            else
-            {
-                Console.WriteLine("\r\nLe labyrinthe n'est pas encore initialisé.");
-            }
-            break;
-        case '3':
-            menu_continue = false;
-            break;
-        default:
-            Console.WriteLine("\r\n" + "Caractère non valide.\r\n");
-            break;
+        Console.WriteLine("Caractère non valide.");
     }
+    else
+    {
+        Chessboard cb = new Chessboard(n);
+        List<List<int>> sol = cb.Resoudre();
+        if (sol.Max(x => x.Count) < n)
+        {
+            Console.WriteLine("Aucune solution dans ce cas.");
+        }
+        else
+        {
+            Console.WriteLine("La solution au problème des n-reines dans ce cas a été générée.");
+            bool board_continue = true;
+            while (board_continue)
+            {
+                Console.WriteLine("\r\nVeuillez entrer un numéro pour sélectionner la fonction que vous voulez utiliser:");
+                Console.WriteLine("(1) Une solution possible pour une taille de plateau donnée avec nombre de tuples k-prometteurs");
+                Console.WriteLine("(2) Toutes les solutions possibles pour une taille de plateau avec nombre de tuples k-prometteurs.");
+                Console.WriteLine("(3) Le nombre de tuples k-prometteurs total pour explorer toute l’espace de recherche.");
+                Console.WriteLine("(4) Entrer un nouveau N.");
+                Console.WriteLine("(5) Quitter.");
+                switch (Console.ReadKey().KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine();
+                        List<int> first = sol.First(x => x.Count == n);
+                        Console.WriteLine("Le nombre de tuples k-prometteurs de la première solution :" + sol.IndexOf(first));
+                        Console.WriteLine("[" + string.Join(",", first.Select(x => x + 1)) + "]");
+                        cb.Afficher(first);
+                        break;
+                    case '2':
+                        Console.WriteLine();
+                        int kcounter = 0;
+                        foreach (List<int> solution in sol)
+                        {
+                            kcounter++;
+                            if (solution.Count == n)
+                            {
+                                Console.WriteLine("Le nombre de tuples k-prometteurs de la solution :" + kcounter);
+                                Console.WriteLine("[" + string.Join(",", solution.Select(x => x + 1)) + "]");
+                                cb.Afficher(solution);
+                                kcounter = 0;
+                            }
+                        }
+                        break;
+                    case '3':
+                        Console.WriteLine();
+                        Console.WriteLine("Le nombre de tuples k-prometteurs total :" + sol.Count);
+                        break;
+                    case '4':
+                        board_continue = false;
+                        break;
+                    case '5':
+                        board_continue = false;
+                        menu_continue = false;
+                        break;
+                    default:
+                        Console.WriteLine("\r\n" + "Caractère non valide.\r\n");
+                        break;
+                }
+            }
+
+        }
+
+    }
+
 }
 
 
